173c7af2b37ec89e6ab1e1ed0403b2dac33dacca
Justin Lee <jusclee@ucsc.edu>
2018-05-10 22:53:52 -0700

    first commit

---
+++ create_nodes.py
@@ -0,0 +1,70 @@
+import os
+import sys
+import time
+# import socket
+import random
+# from chord import *
+from optparse import OptionParser
+
+#settings:
+SIZE = 100000 #note not all nodes will be created because of repeated values due to psuedorandom random function
+
+parser = OptionParser()
+parser.add_option("-n", "--nodes", dest="nodes",
+                  help="number of nodes created within the system", default=1000)
+
+(options, args) = parser.parse_args()
+
+threads = int(options.nodes)
+
+
+class Address(object):
+	def __init__(self, ip, port):
+		self.ip = ip
+		self.port = int(port)
+		self.hash = 0
+		self.id = "%s:%s" % (self.ip,self.port)
+
+	def __hash__(self):
+		self.hash = hash(("%s:%s" % (self.ip, self.port))) % SIZE
+		return hash(("%s:%s" % (self.ip, self.port))) % SIZE
+
+	def __cmp__(self, other):
+		return other.__hash__() < self.__hash__()
+
+	def __eq__(self, other):
+		return other.__hash__() == self.__hash__()
+
+	def __str__(self):
+		return "[\"%s\", \"%s\", %s, %s]" % (self.id, self.ip, self.port, self.hash)
+
+
+def main():
+	parser = OptionParser()
+	parser.add_option("-n", "--nodes", dest="nodes", help="number of nodes created within the system", default=1000)
+	(options, args) = parser.parse_args()
+	nnodes = int(options.nodes)
+
+	print "Creating chord network with : %s nodes" % nnodes
+
+	# create ip, port lists
+	port_list = []
+	ip_list = []
+	for x in range(nnodes):
+		ip_list.append(str(random.randrange(0,255)) + '.' + str(random.randrange(0,255)) + '.' + str(random.randrange(0,255)) + '.' + str(random.randrange(0,255)))
+		port_list.append(random.randrange(1, 65535))
+
+
+	address_list = map(lambda (ip, port): Address(ip, port), zip(ip_list, port_list))
+	hash_list 	 = map(lambda addr: addr.__hash__(), address_list)
+
+	# keep unique ones
+	address_list = sorted(set(address_list))
+	print "final number of addresses : %s" % len(address_list)
+
+
+  
+if __name__== "__main__":
+  main()
+
+
